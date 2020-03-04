b729d67db48a1fb719656dbfeb4bffead9e2dcd6
KSym04 <dev@dopethemes.com>
2020-03-04 16:07:57 +0800

    Added license.

---
+++ gpdr-cookie-notice.php
@@ -0,0 +1,30 @@
+<?php
+/*
+Plugin Name: GDPR Cookie Notice & Compliance
+Plugin URI: https://www.eteam.dk/om-eteam/
+Description: Simple utility plugin for GDPR compliance
+Version: 1.0.0
+Author: Eteam.dk
+Author URI: https://www.eteam.dk/
+Copyright: Eteam.dk
+License: GPLv3
+License URI: https://www.gnu.org/licenses/gpl-3.0.html
+Text Domain: gdprcono
+Domain Path: /lang
+*/
+
+/*
+    Copyright Eteam.dk
+    
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1335, USA
+*/
